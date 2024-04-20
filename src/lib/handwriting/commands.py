095f241dfc81e50dc65c7f87cb963b0077321ea6
Martin Kunze <75624599+maddinkunze@users.noreply.github.com>
2024-04-20 20:29:37 +0200

    updated commands.py to include a laser mode (can be used for milling, laser engraving and servos)

--- src/lib/handwriting/commands.py
+++ src/lib/handwriting/commands.py
@@ -16,3 +16,22 @@ def gcode(pendraw=0, pentravel=5, penpause=50, feeddraw=1000, feedtravel=1000):
             "end": f"G0 Z{penpause} F{feedtravel}\n"
         }
     }
+
+def gcode_laser(pendraw=0, pentravel=None, penpause=None, feeddraw=1000, feedtravel=1000):
+    return {
+        "draw": {
+            "start": f"M3 S{pendraw}",
+            "move": f"G1 X{{x:f}} Y{{y:f}} F{feeddraw}\n",
+            "end": None,
+        },
+        "travel": {
+            "start": f"M5" if pentravel is None else f"M3 S{pentravel}",
+            "move": f"G0 X{{x:f}} Y{{y:f}} F{feedtravel}\n",
+            "end": None
+        },
+        "page": {
+            "start": None,
+            "next": f"M0\n",
+            "end": None
+        }
+    }
